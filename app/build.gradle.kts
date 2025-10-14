bb54eb6afbb566cc46c895e12421785de36208a0
mi-kuzeka <ckatyao@yahoo.com>
2025-10-14 12:18:25 +0300

    Updated versions of libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,4 +96,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    androidTestImplementation(libs.kotlin.test.junit)
 }
