61cd5bb5c56ae6fd64f67a78676b30fab5a7e87b
mi-kuzeka <ckatyao@yahoo.com>
2026-04-10 16:26:41 +0300

    (MYDATES-361) Fixed warning: replaced "TopAppBarDefaults.centerAlignedTopAppBarColors" with "TopAppBarDefaults.topAppBarColors"

--- app/src/main/java/com/kuzepa/mydates/ui/components/TopAppBar.kt
+++ app/src/main/java/com/kuzepa/mydates/ui/components/TopAppBar.kt
@@ -39,7 +39,7 @@ fun TopAppBar(
     endIcon: @Composable () -> Unit = {}
 ) {
     CenterAlignedTopAppBar(
-        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+        colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.secondaryContainer,
             titleContentColor = MaterialTheme.colorScheme.primary,
         ),
